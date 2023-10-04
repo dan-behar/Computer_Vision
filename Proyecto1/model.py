1d756cd24393cb0e4e438ee02e51289216586955
Daniel Behar Aldana <danielbehar@ufm.edu>
2023-10-04 12:47:46 -0600

    ajustando model.py para hacerlo universal otra vez

--- Proyecto1/model.py
+++ Proyecto1/model.py
@@ -2,8 +2,7 @@
 import cv2 as cv
 import os
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifiergit 
 from sklearn.metrics import accuracy_score
 from joblib import dump
 
